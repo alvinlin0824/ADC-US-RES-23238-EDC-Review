2e805cb64f794373922329237cb4bc468d251e8f
alvinlin0824 <alvin.lin@abbott.com>
2023-12-01 11:19:03 -0800

    Commit From RStudio

--- app.R
+++ app.R
@@ -1,13 +1,17 @@
-## Update Add Tabsets on 01-Aug-2022
 library(shiny)
 library(shinyFeedback)
 library(tidyverse)
+library(fs)
 library(rclipboard)
 options(shiny.maxRequestSize = 30*1024^2)
 
+# study event
+event <- dir_ls(gsub("\\\\", "/", r"(\\wf00168p.oneabbott.com\data1\CDM\ADC-US-RES-23238\OpenClinicaExtract\Current)")) |> 
+         str_extract("(?<=Current/).{3}")
+
 ui <- fluidPage(
   theme = bslib::bs_theme(bootswatch = "minty"),
-  titlePanel("InHouse 23238 EDC Review"),
+  titlePanel("ADC-US-RES-23238 EDC Review"),
   useShinyFeedback(),
   a(span("Please email Alvin Lin if you run into any issues",style = "color:black"),href = "mailto:alvin.lin@abbott.com"),
   fluidRow(column(width = 12,
@@ -17,39 +21,58 @@ ui <- fluidPage(
                   rclipboardSetup(),
                   uiOutput("clip"))),
                   br(),
+                  actionButton("reset","Refresh"),
+                  br(),
+                  br(),
   
   tabsetPanel(
             tabPanel(p("Adverse Event Report",style = "font-size:20px;"),
-              fileInput("ae1","Please Upload AE1",accept = ".sas7bdat",width = "400px"),
-              fileInput("ae2","Please Upload AE2",accept = ".sas7bdat",width = "400px")),
+             fluidRow(column(width = 3,
+                      fileInput("ae1","Please Upload AE1",accept = ".sas7bdat",width = "400px")),
+                      column(width = 3,
+                      fileInput("ae2","Please Upload AE2",accept = ".sas7bdat",width = "400px")))),
   
             tabPanel(p("Device Incident Report",style = "font-size:20px;"),
               fileInput("de","Please Upload DE",accept = ".sas7bdat",width = "400px")),
             
             tabPanel(p("Subject Characteristics",style = "font-size:20px;"),
-                     fileInput("dm","Please Upload DM",accept = ".sas7bdat",width = "400px"),
-                     fileInput("vs","Please Upload VS",accept = ".sas7bdat",width = "400px"),
-                     fileInput("ie","Please Upload IE",accept = ".sas7bdat",width = "400px"),
-                     fileInput("mh","Please Upload MH",accept = ".sas7bdat",width = "400px")),
+              fluidRow(column(width = 3,
+                       fileInput("dm","Please Upload DM",accept = ".sas7bdat",width = "400px")),
+                       column(width = 3,
+                       fileInput("vs","Please Upload VS",accept = ".sas7bdat",width = "400px")),
+                       column(width = 3,
+                       fileInput("ie","Please Upload IE",accept = ".sas7bdat",width = "400px")),
+                       column(width = 3,
+                       fileInput("mh","Please Upload MH",accept = ".sas7bdat",width = "400px")))),
             
             tabPanel(p("Sensor Use",style = "font-size:20px;"),
-                     fileInput("du1","Please Upload DU1",accept = ".sas7bdat",width = "400px"),
-                     fileInput("du2","Please Upload DU2",accept = ".sas7bdat",width = "400px"),
-                     fileInput("du3","Please Upload DU3",accept = ".sas7bdat",width = "400px"),
-                     fileInput("du4","Please Upload DU4",accept = ".sas7bdat",width = "400px"),
-                     fileInput("du5","Please Upload DU5",accept = ".sas7bdat",width = "400px")),
+                fluidRow(column(width = 3,
+                         fileInput("du1","Please Upload DU1",accept = ".sas7bdat",width = "400px")),
+                         column(width = 3,
+                         fileInput("du2","Please Upload DU2",accept = ".sas7bdat",width = "400px")),
+                         column(width = 3,
+                         fileInput("du3","Please Upload DU3",accept = ".sas7bdat",width = "400px")),
+                         column(width = 3,
+                         fileInput("du4","Please Upload DU4",accept = ".sas7bdat",width = "400px")),
+                         column(width = 3,
+                         fileInput("du5","Please Upload DU5",accept = ".sas7bdat",width = "400px")))),
             
             tabPanel(p("Protocol Deviation Report",style = "font-size:20px;"),
                      fileInput("pd","Please Upload PD",accept = ".sas7bdat",width = "400px")),
             
             tabPanel(p("Skin Assessment",style = "font-size:20px;"),
-                     fileInput("sk1","Please Upload SK1",accept = ".sas7bdat",width = "400px"),
-                     fileInput("sk2","Please Upload SK2",accept = ".sas7bdat",width = "400px")),
+              fluidRow(column(width = 3,
+                       fileInput("sk1","Please Upload SK1",accept = ".sas7bdat",width = "400px")),
+                       column(width = 3,
+                       fileInput("sk2","Please Upload SK2",accept = ".sas7bdat",width = "400px")))),
             
             tabPanel(p("Adhesive Log",style = "font-size:20px;"),
-                     fileInput("dt2","Please Upload DT2",accept = ".sas7bdat",width = "400px"),
-                     fileInput("dt3","Please Upload DT3",accept = ".sas7bdat",width = "400px"),
-                     fileInput("dt4","Please Upload DT4",accept = ".sas7bdat",width = "400px")),
+              fluidRow(column(width = 3,
+                     fileInput("dt2","Please Upload DT2",accept = ".sas7bdat",width = "400px")),
+                     column(width = 3,
+                     fileInput("dt3","Please Upload DT3",accept = ".sas7bdat",width = "400px")),
+                     column(width = 3,
+                     fileInput("dt4","Please Upload DT4",accept = ".sas7bdat",width = "400px")))),
             
             tabPanel(p("Daily Activity Log",style = "font-size:20px;"),
                      fileInput("dl2","Please Upload DL2",accept = ".sas7bdat",width = "400px")),
@@ -59,8 +82,6 @@ ui <- fluidPage(
             
             ),
   
-
-
    fluidRow(column(width = 12, downloadButton("download","Download Report",class = "btn-block", style = "width:100%;")))
   
 )
@@ -68,13 +89,16 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  # output$text1 <- renderText({input$ae1$datapath})
+  ## Dynamic User interface
+  observeEvent(input$reset,{
+    session$reload()
+  })
   
   ## EDC File Path
   text <- reactive({
     req(input$study)
     
-    exists <- input$study %in% c(str_c("00",as.character(seq(1,5))))
+    exists <- input$study %in% c(event)
     feedbackWarning("study",!exists,"Unkown Study")
     req(exists,cancelOutput = FALSE)
     
@@ -104,7 +128,7 @@ server <- function(input, output, session) {
   ## Download Report
   output$download <- downloadHandler(
     filename = function(){
-      str_c("InHouse 23238 ",input$study," EDC Review Report ",Sys.Date(),".html")
+      str_c("ADC-US-23238 ",input$study," EDC Review Report ",Sys.Date(),".html")
     }, #"InHouse 16157 EDC Review Report.html",
     content = function(file){
       params <- list(Study = input$study,
@@ -137,7 +161,7 @@ server <- function(input, output, session) {
       )
       on.exit(removeNotification(id), add = TRUE)
       
-      rmarkdown::render("EDC-Shiny-Review.Rmd",
+      rmarkdown::render("ADC-US-RES-23238-EDC-Review.Rmd",
                         output_file = file,
                         params = params,
                         envir = new.env(parent = globalenv()))
